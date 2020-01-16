f7077b96c00c9302c8a9c776f45700535d14ba4e
oyj <ouyj@nala.com.cn>
2020-01-16 16:23:06 +0800

    测试

--- src/main/java/com/nala/controller/LotteryController.java
+++ src/main/java/com/nala/controller/LotteryController.java
@@ -4,8 +4,12 @@ import com.nala.entity.Constants;
 import com.nala.entity.Result;
 import com.nala.entity.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * @author oyj
@@ -16,14 +20,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/lottery")
 public class LotteryController {
 
-    @RequestMapping("/login")
-    public Result<Void> login(User user){
+    @PostMapping("/login")
+    public Result<Void> login(User user, HttpServletRequest request) {
+        Result result = new Result();
         String phoneNumber = user.getPhoneNumber();
         User userByPhone = Constants.USER_MAP.get(phoneNumber);
-        if (null != userByPhone && user.getCarNumber().equals(userByPhone.getCarNumber()) && user.getPhoneNumber().equals(userByPhone.getPhoneNumber())){
-                return new Result().defaultSuccess();
+        if (null != userByPhone && userByPhone.getUserName().equals(user.getUserName()) && userByPhone.getCarNumber().equals(user.getCarNumber())) {
+            HttpSession session = request.getSession();
+            session.setAttribute("phoneNumber",phoneNumber);
+            return result.defaultSuccess();
+        } else {
+            result.setSuccess(false,"10001","账户信息错误");
+            return result;
         }
-        return null;
     }
 
     @GetMapping("/xx")
